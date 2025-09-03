d3ff7599a09d72bdf3e86b6f7dd6742f35cab6eb
daviPeter07 <davipetersondev173@gmail.com>
2025-09-03 01:38:58 -0300

    feat: add icon burguer, icon site img

--- src/scripts/global.js
+++ src/scripts/global.js
@@ -1,23 +1,27 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const themeToggleBtn = document.getElementById("theme-toggle");
+  const htmlElement = document.documentElement;
 
-    const themeToggleBtn = document.getElementById('theme-toggle');
-    const htmlElement = document.documentElement;
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme) {
+    htmlElement.setAttribute("data-theme", savedTheme);
+  }
 
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme) {
-        htmlElement.setAttribute('data-theme', savedTheme);
+  themeToggleBtn.addEventListener("click", () => {
+    const currentTheme = htmlElement.getAttribute("data-theme");
+
+    if (currentTheme === "light") {
+      htmlElement.setAttribute("data-theme", "dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      htmlElement.setAttribute("data-theme", "light");
+      localStorage.setItem("theme", "light");
     }
+  });
+});
 
-    themeToggleBtn.addEventListener('click', () => {
-        const currentTheme = htmlElement.getAttribute('data-theme');
-
-        // Alterna o tema
-        if (currentTheme === 'light') {
-            htmlElement.setAttribute('data-theme', 'dark');
-            localStorage.setItem('theme', 'dark');
-        } else {
-            htmlElement.setAttribute('data-theme', 'light');
-            localStorage.setItem('theme', 'light');
-        }
-    });
+const hamburgerBtn = document.getElementById("hamburger-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+hamburgerBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("active");
 });
